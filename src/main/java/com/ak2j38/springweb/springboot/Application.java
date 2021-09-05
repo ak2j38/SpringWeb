14b96313f280d7ddb816c8da7badec5a3555f6f3
ak2j38 <k2j38@naver.com>
2021-09-05 23:50:54 +0900

    Add domain and Test Code

---
+++ src/main/java/com/ak2j38/springweb/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.ak2j38.springweb.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/ak2j38/springweb/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.ak2j38.springweb.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
